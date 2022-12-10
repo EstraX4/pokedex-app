10f8a348be483dec110772f78bb74a6c71d9e03a
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2022-12-10 14:11:40 -0600

    update

---
+++ src/api/pokemon.js
@@ -0,0 +1,20 @@
+import { API_HOST } from "../utils/constant";
+
+export async function getPokemonApi() {
+  try {
+    const url = `${API_HOST}/pokemon?limit=20&offset=0`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getPokemonDetailsByUrlApi() {
+    try {
+
+    } catch (error) {
+        throw error;
+    }
+}
---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import Account from "./Account";
+import Favorite from "./Favorite";
+import Pokedex from "./Pokedex";
+import Pokemon from "./Pokemon";
+
+export { Account, Favorite, Pokedex, Pokemon }
